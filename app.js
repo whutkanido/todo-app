2186639fe9cec235b67c3e38080963455cf67681
whutkanido <flagranthusk@gmail.com>
2021-08-16 10:35:44 -0400

    base

---
+++ app.js
@@ -0,0 +1,17 @@
+// code for getting current value of input box
+// $("#input-box").val()
+
+
+// On button click code
+/*
+* $("button").on("click", event => {
+*    $toDoUserInput = $("input-box").val()
+* })
+*/
+
+
+$(() => {
+
+	// DOM stuff
+
+})
